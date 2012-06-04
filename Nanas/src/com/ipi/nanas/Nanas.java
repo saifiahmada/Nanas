dda3656b5ff5e1e440f868e3acd29d07e25b2487
Saifi Ahmada <saifiahmada@gmail.com>
2012-06-04 19:15:09 +0800

    testing nanas

---
+++ Nanas/src/com/ipi/nanas/Nanas.java
@@ -0,0 +1,37 @@
+package com.ipi.nanas;
+
+public class Nanas {
+	
+	private String nama;
+	
+	private String rasa;
+	
+	private String harga;
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public String getRasa() {
+		return rasa;
+	}
+
+	public void setRasa(String rasa) {
+		this.rasa = rasa;
+	}
+
+	public String getHarga() {
+		return harga;
+	}
+
+	public void setHarga(String harga) {
+		this.harga = harga;
+	}
+	
+	
+
+}
