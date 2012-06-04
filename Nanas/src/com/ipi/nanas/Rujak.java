978cee693183e09e359a4e91fbbcad94dafa91fc
Saifi Ahmada <saifiahmada@gmail.com>
2012-06-04 21:07:04 +0800

    ok campur jadi rujak

--- Nanas/src/com/ipi/nanas/Rujak.java
+++ Nanas/src/com/ipi/nanas/Rujak.java
@@ -1,5 +1,40 @@
 package com.ipi.nanas;
 
 public class Rujak {
+	
+	private Nanas nanas;
+	
+	private Pisang pisang;
+	
+	private Timun timun;
+	
+	public String campur(){
+		
+		return "campur semua jadi rujak";
+	}
 
+	public Nanas getNanas() {
+		return nanas;
+	}
+
+	public void setNanas(Nanas nanas) {
+		this.nanas = nanas;
+	}
+
+	public Pisang getPisang() {
+		return pisang;
+	}
+
+	public void setPisang(Pisang pisang) {
+		this.pisang = pisang;
+	}
+
+	public Timun getTimun() {
+		return timun;
+	}
+
+	public void setTimun(Timun timun) {
+		this.timun = timun;
+	}
+	
 }
