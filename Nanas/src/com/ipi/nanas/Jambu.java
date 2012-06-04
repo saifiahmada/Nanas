b55ea563366e24e8931ac9c8cc8cf66a4bd2092e
Saifi Ahmada <saifiahmada@gmail.com>
2012-06-04 22:05:52 +0800

    jambu

---
+++ Nanas/src/com/ipi/nanas/Jambu.java
@@ -0,0 +1,17 @@
+package com.ipi.nanas;
+
+public class Jambu {
+	
+	private String jambu;
+
+	public String getJambu() {
+		return jambu;
+	}
+
+	public void setJambu(String jambu) {
+		this.jambu = jambu;
+	}
+	
+	
+
+}
