3d939b803942edd589c48dd655bb28c662dd2c2a
Saifi Ahmada <saifiahmada@gmail.com>
2012-06-05 09:20:22 +0800

    commited by saifi ahmada, create class pepaya

---
+++ Nanas/src/com/ipi/nanas/Pepaya.java
@@ -0,0 +1,37 @@
+package com.ipi.nanas;
+
+public class Pepaya {
+	
+	private String pepaya;
+	
+	private String warna;
+	
+	private String rasa;
+
+	public String getPepaya() {
+		return pepaya;
+	}
+
+	public void setPepaya(String pepaya) {
+		this.pepaya = pepaya;
+	}
+
+	public String getWarna() {
+		return warna;
+	}
+
+	public void setWarna(String warna) {
+		this.warna = warna;
+	}
+
+	public String getRasa() {
+		return rasa;
+	}
+
+	public void setRasa(String rasa) {
+		this.rasa = rasa;
+	}
+	
+	
+
+}
