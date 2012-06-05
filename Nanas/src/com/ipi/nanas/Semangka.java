eaa691ecd1fdffdbdd5678d3f11a539399c5d649
Saifi Ahmada <saifiahmada@gmail.com>
2012-06-05 09:31:00 +0800

    commited by saifi ahmada, create class semangka

---
+++ Nanas/src/com/ipi/nanas/Semangka.java
@@ -0,0 +1,35 @@
+package com.ipi.nanas;
+
+public class Semangka {
+	
+	private String semangka;
+	
+	private String warna;
+	
+	private String rasa;
+
+	public String getSemangka() {
+		return semangka;
+	}
+
+	public void setSemangka(String semangka) {
+		this.semangka = semangka;
+	}
+
+	public String getWarna() {
+		return warna;
+	}
+
+	public void setWarna(String warna) {
+		this.warna = warna;
+	}
+
+	public String getRasa() {
+		return rasa;
+	}
+
+	public void setRasa(String rasa) {
+		this.rasa = rasa;
+	}
+	
+}
