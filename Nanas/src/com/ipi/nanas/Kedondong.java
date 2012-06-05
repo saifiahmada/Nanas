6486b6c44eae801195268ee3a2886a62c0fed120
Saifi Ahmada <saifiahmada@gmail.com>
2012-06-05 09:51:07 +0800

    committed by saifi ahmada, create file kedondong dan mangga

---
+++ Nanas/src/com/ipi/nanas/Kedondong.java
@@ -0,0 +1,35 @@
+package com.ipi.nanas;
+
+public class Kedondong {
+	
+	private String kedondong;
+	
+	private String warna;
+	
+	private String rasa;
+
+	public String getKedondong() {
+		return kedondong;
+	}
+
+	public void setKedondong(String kedondong) {
+		this.kedondong = kedondong;
+	}
+
+	public String getWarna() {
+		return warna;
+	}
+
+	public void setWarna(String warna) {
+		this.warna = warna;
+	}
+
+	public String getRasa() {
+		return rasa;
+	}
+
+	public void setRasa(String rasa) {
+		this.rasa = rasa;
+	}
+	
+}
