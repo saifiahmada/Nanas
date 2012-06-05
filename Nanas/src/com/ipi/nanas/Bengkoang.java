b2c64b1d3a4b30a6daf7c4b9d4b24c512ef77f72
Saifi Ahmada <saifiahmada@gmail.com>
2012-06-05 09:11:50 +0800

    new commit from saifi ahmada, class bengkoang

---
+++ Nanas/src/com/ipi/nanas/Bengkoang.java
@@ -0,0 +1,25 @@
+package com.ipi.nanas;
+
+public class Bengkoang {
+	
+	private String bengkoang;
+	
+	private String warna;
+
+	public String getBengkoang() {
+		return bengkoang;
+	}
+
+	public void setBengkoang(String bengkoang) {
+		this.bengkoang = bengkoang;
+	}
+
+	public String getWarna() {
+		return warna;
+	}
+
+	public void setWarna(String warna) {
+		this.warna = warna;
+	}
+
+}
